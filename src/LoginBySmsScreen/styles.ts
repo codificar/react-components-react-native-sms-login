c7fabc6ddbfe71ae66bd51f3b857792c3f436c60
=ricardo.cavalcanti <ricardo.cavalcanti@codficar.com.br>
2021-08-16 16:12:05 -0300

    style fixes

--- src/LoginBySmsScreen/styles.ts
+++ src/LoginBySmsScreen/styles.ts
@@ -2,52 +2,50 @@ import styled from "styled-components/native"
 import { StyleSheet } from "react-native";
 import { TextInputMask, TextInputMaskProps } from 'react-native-masked-text'
 
-let buttonColor = '#3f5105'
-
 export const Container = styled.View`
 flex: 1;
-backgroundColor: #fbfbfb;`;
+backgroundColor: #fbfbfb;`
 
 export const NextText = styled.Text`    
-    fontFamily: 'Roboto',
-    color: '#ffffff',
-    fontSize: 16`;
+    fontFamily: Roboto;
+    color: #ffffff;
+    fontSize: 16;`
 
 
 
 export const TextDesc = styled.Text`
-    fontFamily: 'Roboto',
-    fontSize: 12,
-    color: '#ff5555',
-    marginTop: 5,
-    fontWeight: 'bold'
+    fontFamily: Roboto;
+    fontSize: 12;
+    color: #ff5555;
+    marginTop: 5;
+    fontWeight: bold;
 `;
 
 export const ContValidation = styled.View`
-    alignItems: 'center',
-    marginTop: 20`;
+    alignItems: enter;
+    marginTop: 20;`;
     
 
-export const TextCodeSentTitle = styled.Text`fontFamily: 'Roboto'`;
+export const TextCodeSentTitle = styled.Text`fontFamily: Roboto;`;
 
 export const ButtonStyled = styled.TouchableOpacity`
-        backgroundColor: ${buttonColor},
-        width: '90%',
-        height: 52,
-        borderRadius: 4,
-        justifyContent: 'center',
-        alignItems: 'center',
-        alignSelf: 'center',
-        marginBottom: 10,
-        bottom: 15,
-        marginTop: 100,`;
+        backgroundColor: #3f5105;
+        width: 90%;
+        height: 52;
+        borderRadius: 4;
+        justifyContent: center;
+        alignItems: center;
+        alignSelf: center;
+        marginBottom: 10;
+        bottom: 15;
+        marginTop: 100;`;
 
 export const InputValidationCode = styled.TextInput`
-    borderBottomWidth: 1,
-    borderBottomColor: '#c6cbd4',
-    textAlign: 'center',
-    fontSize: 22,
-    marginLeft: 10,`;
+    borderBottomWidth: 1;
+    borderBottomColor: #c6cbd4;
+    textAlign: center;
+    fontSize: 22;
+    marginLeft: 10;`;
 
     // lib component, not part of the default react library, cant be a styled component.
     
